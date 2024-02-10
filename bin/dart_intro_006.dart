d693f5162ecc3258ff4cea341e719dc596dc965f
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2024-02-10 11:45:37 +0300

    start with data type in dart

--- bin/dart_intro_006.dart
+++ bin/dart_intro_006.dart
@@ -1,5 +1,50 @@
-import 'package:dart_intro_006/dart_intro_006.dart' as dart_intro_006;
+void main() {
+  print('hi from dart');
+  // data types in dart
+  //  String : any caracter inside '' or "" or """ """
+//  int : any number
+//  double : any number that have a decimal point
+//  bool : true or false
+//  null : null value
+//  dynamic : any type
+//  List : multutiple values inside one var [0,1,2,...] > methods
+//  Map : multiple values inside one var {key:value, key:value, } > methods
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_intro_006.calculate()}!');
+//  datatype name = datatype value
+  String username = 'james';
+  print("username = $username");
+
+  int age = 25;
+  print("age = $age");
+
+  double height = 1;
+  print("height = $height");
+
+  bool isMale = true;
+  print("isMale = $isMale");
+
+  bool isFemale = false;
+  print("isFemale = $isFemale");
+
+// null nullValue = null;
+// print ("nullValue = $nullValue");
+
+  dynamic nullValue = null;
+
+  nullValue = 10;
+  print("nullValue = $nullValue");
+  nullValue = '1fdsfsfs0';
+  print("nullValue = $nullValue");
+// dynamic dynamicValue = 10;
+
+  List list = [1, 2, 3, 4, 5, 6, 7, 'fdgfdgdg'];
+  print("list = ${list[5]}");
+  print("list = ${list.length}");
+
+  Map map = {"username": 'james', "age": 25, "height": 1.8, "isMale": false};
+  print("map = ${map[username]}");
+  print("map = ${map.length}");
+
+  var dynamicValue = 'dsfdsfdsf';
+  print(dynamicValue);
 }
